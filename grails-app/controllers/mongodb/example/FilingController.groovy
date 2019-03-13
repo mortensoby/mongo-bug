316d2e9af228652a28b045c968850dfb8391a750
Morten Holm Søby <mhs@nine.dk>
2019-03-13 14:14:47 +0100

    mongoSpec which causes compilation error

---
+++ grails-app/controllers/mongodb/example/FilingController.groovy
@@ -0,0 +1,11 @@
+package mongodb.example
+
+
+import grails.rest.*
+import grails.converters.*
+
+class FilingController {
+	static responseFormats = ['json', 'xml']
+	
+    def index() { }
+}
---
+++ src/test/groovy/mongodb/example/FilingControllerSpec.groovy
@@ -0,0 +1,19 @@
+package mongodb.example
+
+import grails.test.mongodb.MongoSpec
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class FilingControllerSpec extends MongoSpec implements ControllerUnitTest<FilingController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+        true == false
+    }
+}
